a465188ed3576eb5a155022f4ab1797361be8857
kerni714 <kerni714@student.liu.se>
2022-10-25 22:25:17 +0200

    Predr

--- R/methods_rr.R
+++ R/methods_rr.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples data(iris)
-#'           s<-linreg(formula=Petal.Length~Species, data=iris)
+#'           s<-ridgereg(formula=Petal.Length~Species, data=iris,lambda=2)
 #'           print(s)
 #'
 print.ridgereg <- function(x,...){
@@ -81,6 +81,37 @@ predict.ridgereg <- function(object,x,...){
   return(y_pred)
 }
 
+#' Predicted values
+#'
+#' Returns the predicted values
+#'
+#' @param object Refers to the object return by the linear regression function
+#' @param x new data to be predicted
+#' @param ... arguments to be passed to methods
+#'
+#' @return predicted values
+#' @export
+#'
+#' @examples data(iris)
+#'           res<-ridgereg(formula=Petal.Length~Species, data= iris, lambda=2)
+#'           pred(res)
+predr <- function(object,x,...){
+
+  stopifnot(class(object)=="ridgereg")
+
+  beta_hat_1_p <- as.matrix(object$coefficients[-1])
+  beta_hat_0 <- as.matrix(object$coefficients[1])
+
+  if (nargs()==1) {
+    y_pred <- object$coefficients
+  }
+  else if (nargs()>1) {
+    stopifnot(is.data.frame(x),(names(x) %in% names(object$coefficients)[-1]))
+    x <- as.matrix(x)
+    y_pred <- as.vector(sweep(x%*%beta_hat_1_p,2,beta_hat_0,'+'))
+  }
+  return(y_pred)
+}
 
 rmse <- function(error)
 {
